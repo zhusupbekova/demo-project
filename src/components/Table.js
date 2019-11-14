1f24d0b360eb37af05fd65414c88d49c7f623ee4
Alina <zhusupbekovalina@gmail.com>
2019-11-14 16:01:35 +0800

    moved user data

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,39 +2,17 @@ import React from "react";
 import { Table, Layout, Button } from "antd";
 
 import "./Table.css";
+import users from "../data/users";
 
 const { Column } = Table;
 const { Header, Sider, Content, Footer } = Layout;
-
-const data = [
-  {
-    key: "1",
-    name: "Alina",
-    lastName: "Zhusupbekova",
-    username: "zhusupbekovalina"
-  },
-
-  {
-    key: "2",
-    name: "Osman Mesut",
-    lastName: "Ozcan",
-    username: "osmanmesutocan"
-  },
-
-  {
-    key: "3",
-    name: "Necmettin",
-    lastName: "Karakaya",
-    username: "necmettinkarakaya"
-  }
-];
 
 class MainContent extends React.Component {
   render() {
     return (
       <Content className="content">
         Username Data
-        <Table dataSource={data}>
+        <Table dataSource={users}>
           <Column title="Name" dataIndex="name" key="name" />
           <Column title="Last Name" dataIndex="lastName" key="lastname" />
           <Column title="username" dataIndex="username" key="username" />
---
+++ src/data/users.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    key: "1",
+    name: "Alina",
+    lastName: "Zhusupbekova",
+    username: "zhusupbekovalina"
+  },
+
+  {
+    key: "2",
+    name: "Osman Mesut",
+    lastName: "Ozcan",
+    username: "osmanmesutocan"
+  },
+
+  {
+    key: "3",
+    name: "Necmettin",
+    lastName: "Karakaya",
+    username: "necmettinkarakaya"
+  }
+];
