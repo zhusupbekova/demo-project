4a9def7964bdfad35177ce25b5f7ef9b64476ba7
Alina <zhusupbekovalina@gmail.com>
2019-11-20 11:46:57 +0800

    added delete button

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { Table, Button } from "antd";
+import { Table, Button, Avatar } from "antd";
 import reqwest from "reqwest";
 import users from "../data/users";
 import "./Table.css";
 const { Column } = Table;
 
 class TableData extends React.Component {
+  constructor() {
+    super();
+    this.state = { data: users };
+    this.handleDelete = this.handleDelete.bind(this);
+  }
   // constructor() {
   //   super();
   //   this.state = {
@@ -43,17 +48,20 @@ class TableData extends React.Component {
   //   });
   // };
 
-  handleDelete = () => {
+  handleDelete = item => {
     // TODO: handle delete
-    console.log("delete");
+    console.log(item);
+    const data = this.state.data.filter(i => i.id !== item.id);
+    this.setState({ data });
   };
 
   render() {
     return (
       <Table
+        size={"small"}
         className="table"
-        rowSelection={this.props.rowSelection}
-        dataSource={users}
+        // rowSelection={this.props.rowSelection}
+        dataSource={this.state.data}
       >
         <Column title="id" dataIndex="id" key="id" />
         <Column title="openid" dataIndex="openid" key="openid" />
@@ -61,7 +69,12 @@ class TableData extends React.Component {
         <Column title="email" dataIndex="email" key="email" />
         <Column title="nickname" dataIndex="nickname" key="nickname" />
         <Column title="name" dataIndex="name" key="name" />
-        <Column title="headImg" dataIndex="headImg" key="headImg" />
+        <Column
+          title="headImg"
+          dataIndex="headImg"
+          key="headImg"
+          render={(_, row) => <Avatar src={row.headImg} />}
+        />
         <Column title="createdAt" dataIndex="createdAt" key="createdAt" />
         <Column title="updatedAt" dataIndex="updatedAt" key="updatedAt" />
 
@@ -69,8 +82,8 @@ class TableData extends React.Component {
           title="Actions"
           dataIndex=""
           key="x"
-          render={() => (
-            <Button type="link" onClick={this.handleDelete}>
+          render={(_, row) => (
+            <Button type="link" onClick={() => this.handleDelete(row)}>
               Delete
             </Button>
           )}
