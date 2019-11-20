11562a6de3a0a31d7e9002bf10b6987afc088fe1
Alina <zhusupbekovalina@gmail.com>
2019-11-20 15:24:23 +0800

    added edit button

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,52 +1,206 @@
 import React from "react";
-import { Table, Button, Avatar } from "antd";
-import reqwest from "reqwest";
+import {
+  Table,
+  Button,
+  Avatar,
+  Input,
+  InputNumber,
+  Popconfirm,
+  Form
+} from "antd";
+// import reqwest from "reqwest";
 import users from "../data/users";
 import "./Table.css";
 const { Column } = Table;
 
-class TableData extends React.Component {
-  constructor() {
-    super();
-    this.state = { data: users };
-    this.handleDelete = this.handleDelete.bind(this);
+const EditableContext = React.createContext();
+
+class EditableCell extends React.Component {
+  getInput = () => {
+    if (this.props.inputType === "number") {
+      return <InputNumber />;
+    }
+    return <Input />;
+  };
+
+  renderCell = args => {
+    const { getFieldDecorator } = args;
+
+    const {
+      editing,
+      dataIndex,
+      title,
+      inputType,
+      record,
+      index,
+      children,
+      ...restProps
+    } = this.props;
+
+    return (
+      <td {...restProps}>
+        {editing ? (
+          <Form.Item style={{ margin: 0 }}>
+            {getFieldDecorator(dataIndex, {
+              rules: [
+                {
+                  required: true,
+                  message: `Please Input ${title}!`
+                }
+              ],
+              initialValue: record[dataIndex]
+            })(this.getInput())}
+          </Form.Item>
+        ) : (
+          children
+        )}
+      </td>
+    );
+  };
+
+  render() {
+    return (
+      <EditableContext.Consumer>{this.renderCell}</EditableContext.Consumer>
+    );
   }
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     loading: false,
-  //     data: [],
-  //     pagination: {}
-  //   };
-  // }
-  // async componentDidMount() {
-  //   await this.fetch();
-  // }
+}
+class TableData extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleDelete = this.handleDelete.bind(this);
 
-  // fetch = async (params = {}) => {
-  //   console.log("params:", params);
-  //   this.setState({ loading: true });
-  //   const data = await reqwest({
-  //     url: "https://randomuser.me/api",
-  //     method: "get",
-  //     data: {
-  //       results: 10,
-  //       ...params
-  //     },
-  //     type: "json"
-  //   });
+    this.state = {
+      data: users.map(user => {
+        user.key = user.id;
+        return user;
+      }),
+      editingKey: ""
+    };
 
-  //   console.log("my data", data);
-  //   const pagination = { ...this.state.pagination };
-  //   // Read total count from server
-  //   pagination.total = data.totalCount;
-  //   // pagination.total = 200;
-  //   this.setState({
-  //     loading: false,
-  //     data: data.results,
-  //     pagination
-  //   });
-  // };
+    this.columns = [
+      {
+        title: "id",
+        dataIndex: "id",
+        width: "5%"
+      },
+      {
+        title: "openedid",
+        dataIndex: "openedid",
+        width: "15%",
+        editable: true
+      },
+      {
+        title: "email",
+        dataIndex: "email",
+        width: "15%",
+        editable: true
+      },
+      {
+        title: "nickname",
+        dataIndex: "nickname",
+        width: "10%",
+        editable: true
+      },
+      {
+        title: "name",
+        dataIndex: "name",
+        width: "10%",
+        editable: true
+      },
+      {
+        title: "headImg",
+        dataIndex: "headImg",
+        width: "5%",
+        editable: true,
+        render: (_, row) => <Avatar src={row.headImg} />
+      },
+      {
+        title: "createdAt",
+        dataIndex: "createdAt",
+        width: "10%"
+      },
+      {
+        title: "updatedAt",
+        dataIndex: "updatedAt",
+        width: "10%"
+      },
+      {
+        title: "operation",
+        dataIndex: "operation",
+        render: (text, record) => {
+          const { editingKey } = this.state;
+          const editable = this.isEditing(record);
+          return editable ? (
+            <span>
+              <EditableContext.Consumer>
+                {form => (
+                  <a
+                    onClick={() => this.save(form, record.key)}
+                    style={{ marginRight: 8 }}
+                  >
+                    Save
+                  </a>
+                )}
+              </EditableContext.Consumer>
+              <Popconfirm
+                title="Sure to cancel?"
+                onConfirm={() => this.cancel(record.key)}
+              >
+                <a>Cancel</a>
+              </Popconfirm>
+            </span>
+          ) : (
+            <a
+              disabled={editingKey !== ""}
+              onClick={() => this.edit(record.key)}
+            >
+              Edit
+            </a>
+          );
+        }
+      },
+      {
+        title: "action",
+        dataIndex: "",
+        key: "x",
+        render: (_, row) => (
+          <Button type="link" onClick={() => this.handleDelete(row)}>
+            Delete
+          </Button>
+        )
+      }
+    ];
+  }
+  isEditing = record => record.key === this.state.editingKey;
+
+  cancel = () => {
+    this.setState({ editingKey: "" });
+  };
+
+  save(form, key) {
+    form.validateFields((error, row) => {
+      if (error) {
+        return;
+      }
+      const newData = [...this.state.data];
+      const index = newData.findIndex(item => key === item.key);
+      if (index > -1) {
+        const item = newData[index];
+        newData.splice(index, 1, {
+          ...item,
+          ...row
+        });
+        this.setState({ data: newData, editingKey: "" });
+      } else {
+        newData.push(row);
+        this.setState({ data: newData, editingKey: "" });
+      }
+    });
+  }
+
+  edit(key) {
+    this.setState({ editingKey: key });
+  }
 
   handleDelete = item => {
     // TODO: handle delete
@@ -56,41 +210,42 @@ class TableData extends React.Component {
   };
 
   render() {
-    return (
-      <Table
-        size={"small"}
-        className="table"
-        // rowSelection={this.props.rowSelection}
-        dataSource={this.state.data}
-      >
-        <Column title="id" dataIndex="id" key="id" />
-        <Column title="openid" dataIndex="openid" key="openid" />
-        <Column title="phone" dataIndex="phone" key="phone" />
-        <Column title="email" dataIndex="email" key="email" />
-        <Column title="nickname" dataIndex="nickname" key="nickname" />
-        <Column title="name" dataIndex="name" key="name" />
-        <Column
-          title="headImg"
-          dataIndex="headImg"
-          key="headImg"
-          render={(_, row) => <Avatar src={row.headImg} />}
-        />
-        <Column title="createdAt" dataIndex="createdAt" key="createdAt" />
-        <Column title="updatedAt" dataIndex="updatedAt" key="updatedAt" />
+    const components = {
+      body: {
+        cell: EditableCell
+      }
+    };
 
-        <Column
-          title="Actions"
-          dataIndex=""
-          key="x"
-          render={(_, row) => (
-            <Button type="link" onClick={() => this.handleDelete(row)}>
-              Delete
-            </Button>
-          )}
+    const columns = this.columns.map(col => {
+      if (!col.editable) {
+        return col;
+      }
+      return {
+        ...col,
+        onCell: record => ({
+          record,
+          inputType: col.dataIndex === "age" ? "number" : "text",
+          dataIndex: col.dataIndex,
+          title: col.title,
+          editing: this.isEditing(record)
+        })
+      };
+    });
+    return (
+      <EditableContext.Provider value={this.props.form}>
+        <Table
+          components={components}
+          bordered
+          dataSource={this.state.data}
+          columns={columns}
+          rowClassName="editable-row"
+          pagination={{
+            onChange: this.cancel
+          }}
         />
-      </Table>
+      </EditableContext.Provider>
     );
   }
 }
 
-export default TableData;
+export default Form.create()(TableData);
